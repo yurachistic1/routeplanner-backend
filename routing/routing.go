4885c959749b55c8d2e24c4d4c40358eebe25627
Yury Chistov <yurachistic@gmail.com>
2021-10-07 09:18:57 +0100

    optimize route creation algorithm

--- routing/routing.go
+++ routing/routing.go
@@ -3,36 +3,47 @@ package routing
 import (
 	"fmt"
 	"math"
+	"math/rand"
 )
 
-func CreateRoute(lat, lon, distance, initBearing float64, g Graph) Route {
-	start := closestNode(lat, lon, g)
+func CreateRoute(start *Node, distance, initBearing float64, g Graph) Route {
 
-	route := Route{Path: []*Node{g[start]}, Length: 0, Turns: 0}
+	route := Route{Path: make([]*Node, 1, 1000), Length: 0, Turns: 0}
+	route.Path[0] = start
 
 	var b float64 = initBearing
 	var r float64 = distance / (2 * math.Pi)
 
 	// vars to figure out if next node counts as a turn or not
-	var currentBearing float64
+	var currentBearing float64 = initBearing
 	var newBearing float64
 
+	var previousNode *Node = &Node{Id: -1}
+
 	for route.Length < distance {
-		previousNode := Id(-1)
 		currentNode := route.Path[len(route.Path)-1]
 
 		if len(route.Path) > 1 {
-			previousNode = route.Path[len(route.Path)-2].Id
-			currentBearing = g[previousNode].Edges[currentNode.Id].Bearing
+			previousNode = route.Path[len(route.Path)-2]
+			currentBearing = previousNode.Edges[currentNode.Id].Bearing
 
 		}
 
-		nextId := pickAlongBearing(b, currentNode.Edges, previousNode)
+		steer := pickAlongBearing(b, currentNode.Edges, previousNode.Id)
+		straight := pickAlongBearing(currentBearing, currentNode.Edges, previousNode.Id)
 
-		route.Path = append(route.Path, g[nextId])
-		route.Length += currentNode.Edges[nextId].Distance
+		pick := 0
+		choices := []Id{steer, straight}
 
-		newBearing = g[currentNode.Id].Edges[nextId].Bearing
+		if straight != steer {
+			pick = rand.Intn(2)
+
+		}
+
+		route.Path = append(route.Path, (g)[choices[pick]])
+		route.Length += currentNode.Edges[choices[pick]].Distance
+
+		newBearing = (g)[currentNode.Id].Edges[choices[pick]].Bearing
 
 		if len(route.Path) > 1 && bearingDifference(currentBearing, newBearing) > 80 {
 			route.Turns++
@@ -45,7 +56,7 @@ func CreateRoute(lat, lon, distance, initBearing float64, g Graph) Route {
 }
 
 // PickAlongBearing selects a an edge (connected node id) that has the closest bearing
-// to the target bearing and returns a boolean indicating whether it was a turn or not.
+// to the target bearing.
 func pickAlongBearing(target float64, vals map[Id]Edge, exclude Id) (closest Id) {
 
 	minDifference := math.MaxFloat64
@@ -66,24 +77,6 @@ func pickAlongBearing(target float64, vals map[Id]Edge, exclude Id) (closest Id)
 	return
 }
 
-// ClosestNode returns a node id with the shortest distance to given lat and lon coordinates.
-func closestNode(lat float64, lon float64, g Graph) Id {
-
-	target := Node{Lat: lat, Lon: lon}
-	minDistance := math.MaxFloat64
-	closest := Id(-1)
-
-	for key, val := range g {
-		distance := haversine(&target, val)
-
-		if distance < minDistance {
-			minDistance = distance
-			closest = key
-		}
-	}
-	return closest
-}
-
 func (r *Route) ToPolyline() string {
 	str := "var latlngs2 = [\n"
 
@@ -94,4 +87,21 @@ func (r *Route) ToPolyline() string {
 	}
 
 	return str + "]"
+}
+
+// ClosestNode returns a node pointer with the shortest distance to given lat and lon coordinates.
+func ClosestNode(lat float64, lon float64, g Graph) (closest *Node) {
+
+	target := Node{Lat: lat, Lon: lon}
+	minDistance := math.MaxFloat64
+
+	for _, val := range g {
+		distance := haversine(&target, val)
+
+		if distance < minDistance {
+			minDistance = distance
+			closest = val
+		}
+	}
+	return closest
 }
