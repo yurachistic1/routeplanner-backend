0f8401e38164cd9581be8152a82ea075608d4b67
Yury Chistov <yurachistic@gmail.com>
2021-10-08 13:14:34 +0100

    add functions to create a collection of routes

--- routing/routing.go
+++ routing/routing.go
@@ -4,10 +4,81 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+
+	"github.com/yurachistic1/routeplanner-backend/overpass"
 )
 
+func TopRoutes(lat, lon, distance float64, res overpass.Response) Routes {
+
+	graph := OSMToGraph(res)
+
+	start := ClosestNode(lat, lon, graph)
+
+	top25 := make(Routes, 0, 25)
+
+	for i := 0; i < 360; i += 15 {
+
+		for j := 0; j < 50; j++ {
+			r1 := createRoute(start, distance, float64(i), graph, Clockwise)
+			top25 = appendRoute(r1, top25)
+			r2 := createRoute(start, distance, float64(i), graph, Anticlockwise)
+			top25 = appendRoute(r2, top25)
+		}
+	}
+
+	return top25
+}
+
+func appendRoute(route Route, routes Routes) Routes {
+
+	if len(routes) == 0 {
+		return append(routes, route)
+	}
+
+	if len(routes) == cap(routes) {
+		lastTwo := Routes{routes[len(routes)-1], route}
+
+		if lastTwo.Less(0, 1) {
+			return routes
+		}
+	}
+
+	for i := 0; i < len(routes); i++ {
+		similarity := routeSimilarity(route, routes[i])
+
+		if similarity > 80 {
+			pair := Routes{routes[i], route}
+			if pair.Less(1, 0) {
+				routes[i] = route
+
+			}
+
+			return routes
+		}
+	}
+
+	if len(routes) < cap(routes) {
+		routes = append(routes, route)
+	} else {
+		routes[len(routes)-1] = route
+	}
+
+	for i := len(routes) - 2; i >= 0; i-- {
+		pair := Routes{routes[i], routes[i+1]}
+
+		if pair.Less(0, 1) {
+			return routes
+		} else {
+			routes.Swap(i, i+1)
+		}
+
+	}
+
+	return routes
+}
+
 // Create route returns a circular Route of desired distance at a specified start location.
-func CreateRoute(start *Node, distance, initBearing float64, g Graph, rot Rotation) Route {
+func createRoute(start *Node, distance, initBearing float64, g Graph, rot Rotation) Route {
 
 	route := Route{Path: make([]*Node, 1, 1000), Length: 0, Turns: 0}
 	route.Path[0] = start
